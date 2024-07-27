fd7d1b64fd8972fa0f4be5b73a958dce19eea3a3
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-07-27 15:49:33 +0200

    feat(hypr): Update keybind help

--- .config/hypr/scripts/KeyHints.sh
+++ .config/hypr/scripts/KeyHints.sh
@@ -37,43 +37,45 @@ yad --width=$dynamic_width --height=$dynamic_height \
     --column=Description: \
     --column=Command: \
     --timeout-indicator=bottom \
-"ESC" "close this app" "" "=" "SUPER KEY (Windows Key)" "(SUPER KEY)" \
-" enter" "Terminal" "(kitty)" \
-" SHIFT enter" "DropDown Terminal" "(kitty-pyprland)" \
-" A" "Desktop Overview" "(AGS Overview)" \
-" D" "App Launcher" "(rofi-wayland)" \
-" T" "Open File Manager" "(Thunar)" \
-" S" "Google Search" "(rofi)" \
-" Q" "close active window" "(not kill)" \
-" Shift Q " "kills an active window" "(kill)" \
-" Z" "Desktop Zoom" "(pyprland)" \
-" Alt V" "Clipboard Manager" "(cliphist)" \
-" W" "Choose wallpaper" "(Wallpaper Menu)" \
-" Shift W" "Choose wallpaper effects" "(imagemagick + swww)" \
-"CTRL ALT W" "Random wallpaper" "(via swww)" \
-" B" "Hide/UnHide Waybar" "waybar" \
-" CTRL B" "Choose waybar styles" "(waybar styles)" \
-" ALT B" "Choose waybar layout" "(waybar layout)" \
-" ALT R" "Reload Waybar swaync Rofi" "CHECK NOTIFICATION FIRST!!!" \
-" SHIFT N" "Launch Notification Panel" "swaync Notification Center" \
-" Print" "screenshot" "(grim)" \
-" Shift Print" "screenshot region" "(grim + slurp)" \
-" Shift S" "screenshot region" "(swappy)" \
-" CTRL Print" "screenshot timer 5 secs " "(grim)" \
-" CTRL SHIFT Print" "screenshot timer 10 secs " "(grim)" \
-"ALT Print" "Screenshot active window" "active window only" \
-"CTRL ALT P" "power-menu" "(wlogout)" \
-"CTRL ALT L" "screen lock" "(hyprlock)" \
-"CTRL ALT Del" "Hyprland Exit" "(SAVE YOUR WORK!!!)" \
-" F" "Fullscreen" "Toggles to full screen" \
-" ALT L" "Toggle Dwindle | Master Layout" "Hyprland Layout" \
-" Shift F" "Toggle float" "single window" \
-" ALT F" "Toggle all windows to float" "all windows" \
-" Shift B" "Toggle Blur" "normal or less blur" \
-" SHIFT G" "Gamemode! All animations OFF or ON" "toggle" \
-" ALT E" "Rofi Emoticons" "Emoticon" \
-" ALT V" "Clipboard Manager" "cliphist" \
-" H" "Launch this app" "" \
-" E" "View or EDIT Keybinds, Settings, Monitor" "" \
-"" "" "" \
-"More tips:" "https://github.com/JaKooLit/Hyprland-Dots/wiki" ""\
+    "ESC" "close this app" "" "=" "SUPER KEY (Windows Key)" "(SUPER KEY)" \
+    " enter" "Terminal" "(kitty)" \
+    " SHIFT enter" "DropDown Terminal" "(kitty-pyprland)" \
+    " A" "Desktop Overview" "(AGS Overview)" \
+    " D" "App Launcher" "(rofi-wayland)" \
+    " T" "Open File Manager" "(Thunar)" \
+    " S" "Google Search" "(rofi)" \
+    " Q" "close active window" "(not kill)" \
+    " Shift Q " "kills an active window" "(kill)" \
+    " Z" "Desktop Zoom" "(pyprland)" \
+    " Alt V" "Clipboard Manager" "(cliphist)" \
+    " W" "Choose wallpaper" "(Wallpaper Menu)" \
+    " Shift W" "Choose wallpaper effects" "(imagemagick + swww)" \
+    " B" "Hide/UnHide Waybar" "waybar" \
+    " CTRL B" "Choose waybar styles" "(waybar styles)" \
+    " ALT B" "Choose waybar layout" "(waybar layout)" \
+    " ALT R" "Reload Waybar swaync Rofi" "check notifications first" \
+    " SHIFT N" "Launch Notification Panel" "swaync Notification Center" \
+    " Print" "screenshot" "(grim)" \
+    " Shift Print" "screenshot region" "(grim + slurp)" \
+    " Shift S" "screenshot region" "(swappy)" \
+    " CTRL Print" "screenshot timer 5 secs " "(grim)" \
+    " CTRL SHIFT Print" "screenshot timer 10 secs " "(grim)" \
+    "ALT Print" "Screenshot active window" "active window only" \
+    "CTRL ALT P" "power-menu" "(wlogout)" \
+    "CTRL ALT L" "screen lock" "(hyprlock)" \
+    "CTRL ALT Del" "Hyprland Exit" "(SAVE YOUR WORK!!!)" \
+    " F" "Fullscreen" "Toggles to full screen" \
+    " ALT L" "Toggle Dwindle | Master Layout" "Hyprland Layout" \
+    " Shift F" "Toggle float" "single window" \
+    " ALT F" "Toggle all windows to float" "all windows" \
+    " Shift B" "Toggle Blur" "normal or less blur" \
+    " SHIFT Z" "Zen mode. Enables or disables animations" "toggle" \
+    " ALT E" "Emoji Picker" "Emoticon" \
+    " ALT V" "Clipboard Manager" "cliphist" \
+    " HYPERKEY+N" "Open or focus Notion" "" \
+    " HYPERKEY+S" "Open or focus Wezterm" "" \
+    " HYPERKEY+A" "Open or focus the Browser" "" \
+    " H" "Launch this app" "" \
+    " E" "View or EDIT Keybinds, Settings, Monitor" "" \
+    "" "" "" \
+    "More tips:" "https://github.com/JaKooLit/Hyprland-Dots/wiki" ""
